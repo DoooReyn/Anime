13a860f2468411dc0a4631012b01c35222c31c4b
Reyn <jl88744653@gmail.com>
2017-11-15 23:13:20 +0800

    Update TestCase.lua

--- TestCase.lua
+++ TestCase.lua
@@ -1,15 +1,15 @@
 require('Gloabl')
 
 local sp = cc.Sprite:create('xxx.png')
-	self:addChild(sp)
-	sp:setPosition(display.center)
+self:addChild(sp)
+sp:setPosition(display.center)
 
-	local order = OrderMap(
-		'spawn', OrderMap('moveby', {0.5, cc.p(50,0) }, 'scaleto', {0.5, 1.105}),
-		'spawn', OrderMap('moveby', {0.5, cc.p(-50,0)}, 'scaleto', {0.5, 1.0  }),
-		'spawn', OrderMap('moveby', {0.5, cc.p(-50,0)}, 'scaleto', {0.5, 1.105}),
-		'spawn', OrderMap('moveby', {0.5, cc.p(50,0) }, 'scaleto', {0.5, 1.0  })
-	)
-	local anime = require('Anime')
-	local act = anime(order, {turns = 10})
-	sp:runAction(act)
+local order = OrderMap(
+	'spawn', OrderMap('moveby', {0.5, cc.p(50,0) }, 'scaleto', {0.5, 1.105}),
+	'spawn', OrderMap('moveby', {0.5, cc.p(-50,0)}, 'scaleto', {0.5, 1.0  }),
+	'spawn', OrderMap('moveby', {0.5, cc.p(-50,0)}, 'scaleto', {0.5, 1.105}),
+	'spawn', OrderMap('moveby', {0.5, cc.p(50,0) }, 'scaleto', {0.5, 1.0  })
+)
+local anime = require('Anime')
+local act = anime(order, {turns = 10})
+sp:runAction(act)
